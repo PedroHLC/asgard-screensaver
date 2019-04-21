050678b6963b14cee6a80bbe3e0bc04a916445c5
PedroHLC <root@pedrohlc.com>
2019-04-21 19:55:40 -0300

    Asgard

--- ad.py
+++ ad.py
@@ -54,33 +54,38 @@ def get_color(x, y, t):
 
 
 IMG = ["""
-                  `,:;'''++''';:,`
-              ,'++++############++++',
-           :'++######################++':
-        `'++########++';::;;;''++++#####++'`
-       '+########++;`            .:'++#####+'
-     :++#######+'`                   ,++####++:
-    ;+#######+'`         .,,,.`        ;+#####+;
-   '+#####+'''       ,'+++++++++'`      ,+#####+'
-  ;+######+';     `'++##++''''++++:      '+#####+;
- ,+######+';     ;+###++,      .'++:    ,++######+,
- '+######++.    ;+###+'    :''''+++'    ,++######+'
-.++######+'    ,+###+'   :++####+';'    `++######++.
-:+#######+'    ;+###+;   '+#####+++'    ;++#######+:
-:+#######+'    ;+###+;   :++++++'''`   ,++########+:
-,+#######+'    :+###++.   .'',`;':    ,+##########+,
-.++######++.   .++##++,`     `       '+##########++.
- '+#######+;     '+####+'         :'+############+'
- ,++######++,    `'+#####+++'''+++##############++,
-  :+#######++.    :+############################+:
-   ;+#######++.    ,++#########################+;
-    :+########+;     ;++######################+:
-     .'+#######++,     :'++#################+'.
-       :++#######++;      :'+#############++:
-         :++########++':`  :++##########++:
-           .;+++########+++#########+++;.
-               :'+++++########+++++':
-                    .,:;;;;;;:,.
+                            ```                           
+                    ./oydNMMMMMMNmdyo/.
+                `/smMMmhso+++ooshmNMMMMd+`
+              :yNMNy:`            ``/hNMMNy-
+            -hMMy:`            /Nd.   `/dMMMy.
+           /NMd/-://+++//:-`  :MMMy      /dMMm:
+          :NMMNNNMMMMMMMMMNNmyNMMMM.      `sMMN-
+        -hNMMMMMMMMMMMMMMMMMMMMMMMMy        yMMd
+      `sNMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .NMM-
+     -mMMMMMhs+/:-:+oyhmMMMMMMMMMMMMs        sMM+
+    `mMMMMMs    :ymNMMMMMMMMMMMMMMMMN`       :MMo
+    +MMMyhMy  .yNMMMMMMMMMMMMMMMMMMMMo       +MM/
+    sMMd`:Mm `mMMNdddddNMMMMMMMMMMMMMN`      dMN`
+    /MMs  yMo/MMo.`````.:mMMMNMMMMMMMMo     +MMs
+    `dMm. `hMmM+        `mMMd/MMMMMMMMN.   /NMh`
+     .hMm/``+dMdo:-..:+syMMN- mMMMMMMMMy `sNMo`
+      `/dNy  `:ohdddhdhhMMMs  +MMMMMMMMMomMm/
+        `..  `.:/oyyys+mMMN`  `NMMMMMMMMMNs.
+          `:sdNMMMMMMMMMMMN/.  sMMMMMMMMM-
+         -hMMMMMMMMMMMMMMMMMms-.NMMMMMMMM/
+        -NMMms/:::/sdMMMMMMMMMNssMMMMMMMMd
+        dMMo`       +MMMMMMMMMMMNMMMMMMMMM/
+        MMy        .NMMyNMMMMMMMMhMMMMMMMMm
+        dMd        hMMd`-NMMMMMMs hMMMMMMMM+
+        -mN:      :MMM.  oMMMMMs  :MMMMMMMMN`       `::`
+         `.      `mMMo   `mMMMd`   dMMMMMMMMy     .omMMs
+       `:+osso+//yMMh     oMMM-    :MMMMMMMMMy-./hNMMmo`
+     :yNMMMMMMMMMMMN.     .MMy      yMMMMMMMMMMMMMNh/`
+   `yMMMMMMMMMMMMMM+       NMo      `hMMMMMMMMMMmo.
+  .mMMNNNmmNMMMMMMd        :+.        sMMMMMMMd/`
+ .mMd/.`   `:+sdMN.                    -hMMMh-
+ +my`           ..                       -:-
 """]
 
 
