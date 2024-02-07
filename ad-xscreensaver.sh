1dd690966482cbfee8daa0dafe27dd6df95e7c79
PedroHLC ☭ <root@pedrohlc.com>
2024-02-07 12:29:00 -0300

    ChatGPT rewrote it in Rust (with tons of adjusts); Changed colors

--- ad-xscreensaver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd "${0%/*}"
-xterm -geometry 1360x768 -bg Black -fg White -into $XSCREENSAVER_WINDOW -e ./ad.py
---
+++ img.rs
@@ -0,0 +1,36 @@
+pub const IMG: [&str; 1] = [
+    "
+                            ```
+                    ./oydNMMMMMMNmdyo/.
+                `/smMMmhso+++ooshmNMMMMd+`
+              :yNMNy:`            ``/hNMMNy-
+            -hMMy:`            /Nd.   `/dMMMy.
+           /NMd/-://+++//:-`  :MMMy      /dMMm:
+          :NMMNNNMMMMMMMMMNNmyNMMMM.      `sMMN-
+        -hNMMMMMMMMMMMMMMMMMMMMMMMMy        yMMd
+      `sNMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .NMM-
+     -mMMMMMhs+/:-:+oyhmMMMMMMMMMMMMs        sMM+
+    `mMMMMMs    :ymNMMMMMMMMMMMMMMMMN`       :MMo
+    +MMMyhMy  .yNMMMMMMMMMMMMMMMMMMMMo       +MM/
+    sMMd`:Mm `mMMNdddddNMMMMMMMMMMMMMN`      dMN`
+    /MMs  yMo/MMo.`````.:mMMMNMMMMMMMMo     +MMs
+    `dMm. `hMmM+        `mMMd/MMMMMMMMN.   /NMh`
+     .hMm/``+dMdo:-..:+syMMN- mMMMMMMMMy `sNMo`
+      `/dNy  `:ohdddhdhhMMMs  +MMMMMMMMMomMm/
+        `..  `.:/oyyys+mMMN`  `NMMMMMMMMMNs.
+          `:sdNMMMMMMMMMMMN/.  sMMMMMMMMM-
+         -hMMMMMMMMMMMMMMMMMms-.NMMMMMMMM/
+        -NMMms/:::/sdMMMMMMMMMNssMMMMMMMMd
+        dMMo`       +MMMMMMMMMMMNMMMMMMMMM/
+        MMy        .NMMyNMMMMMMMMhMMMMMMMMm
+        dMd        hMMd`-NMMMMMMs hMMMMMMMM+
+        -mN:      :MMM.  oMMMMMs  :MMMMMMMMN`       `::`
+         `.      `mMMo   `mMMMd`   dMMMMMMMMy     .omMMs
+       `:+osso+//yMMh     oMMM-    :MMMMMMMMMy-./hNMMmo`
+     :yNMMMMMMMMMMMN.     .MMy      yMMMMMMMMMMMMMNh/`
+   `yMMMMMMMMMMMMMM+       NMo      `hMMMMMMMMMMmo.
+  .mMMNNNmmNMMMMMMd        :+.        sMMMMMMMd/`
+ .mMd/.`   `:+sdMN.                    -hMMMh-
+ +my`           ..                       -:-
+"
+];
